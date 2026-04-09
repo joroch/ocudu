1138c64a4550305dc28e6de3c775299567e2f673
Franciszek Maroszek <frank@srs.io>
2026-04-09 21:38:10 +0200

    cu: add missing expert_execution section to cu app YAML writer
    Signed-off-by: Franciszek Maroszek <frank@srs.io>

--- apps/cu/cu_appconfig_yaml_writer.cpp
+++ apps/cu/cu_appconfig_yaml_writer.cpp
@@ -15,6 +15,31 @@
 
 using namespace ocudu;
 
+static void fill_cu_appconfig_expert_execution_section(YAML::Node node, const expert_execution_appconfig& config)
+{
+  {
+    YAML::Node affinities_node = node["affinities"];
+
+    if (config.affinities.main_pool_cpu_cfg.mask.any()) {
+      affinities_node["main_pool_cpus"] =
+          fmt::format("{:,}", span<const size_t>(config.affinities.main_pool_cpu_cfg.mask.get_cpu_ids()));
+    }
+    affinities_node["main_pool_pinning"] = to_string(config.affinities.main_pool_cpu_cfg.pinning_policy);
+  }
+
+  {
+    YAML::Node threads_node   = node["threads"];
+    YAML::Node main_pool_node = threads_node["main_pool"];
+    if (config.threads.main_pool.nof_threads.has_value()) {
+      main_pool_node["nof_threads"] = config.threads.main_pool.nof_threads.value();
+    } else {
+      main_pool_node["nof_threads"] = "auto";
+    }
+    main_pool_node["task_queue_size"] = config.threads.main_pool.task_queue_size;
+    main_pool_node["backoff_period"]  = config.threads.main_pool.backoff_period;
+  }
+}
+
 static void fill_cu_appconfig_remote_control_section(YAML::Node node, const remote_control_appconfig& config)
 {
   node["enabled"]   = config.enabled;
@@ -44,6 +69,7 @@ void ocudu::fill_cu_appconfig_in_yaml_schema(YAML::Node& node, const cu_appconfi
   app_services::fill_buffer_pool_config_in_yaml_schema(node, config.buffer_pool_config);
   fill_logger_appconfig_in_yaml_schema(node, config.log_cfg);
   fill_tracer_appconfig_in_yaml_schema(node, config.trace_cfg);
+  fill_cu_appconfig_expert_execution_section(node["expert_execution"], config.expert_execution_cfg);
   fill_cu_appconfig_remote_control_section(node["remote_control"], config.remote_control_config);
   fill_cu_appconfig_f1ap_section(node, config.f1ap_cfg);
   fill_cu_appconfig_f1u_section(node, config.f1u_cfg);
